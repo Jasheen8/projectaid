aa34bc4605292d8179a8a9e965aaec7bcbbbd216
JasheenAnsari <105618089+Jasheen8@users.noreply.github.com>
2026-01-29 10:51:37 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Smooth scroll for CTA links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e){
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
